2ca63b018158ad4100526ea9316a86ece9d59701
Flávio de oliveira da silva <flaviogamer7007@gmail.com>
2023-04-13 23:15:54 -0300

    python

--- introdução/aula02.py
+++ introdução/aula02.py
@@ -1,3 +1,7 @@
 #x = input(str('qual seu nome?'))
 #print(x)
 
+nome = input("Por favor digite seu nome:")
+anoNascimento = input("Por favor digite seu ano de anscimento:")
+idade = 2020 - int(anoNascimento)
+print("Olá {} sua idade é {} anos".format(nome,idade))
